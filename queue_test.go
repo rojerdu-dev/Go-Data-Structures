74d90b7610d81db5d24f2905fed3654a5255c8e4
Rojer Du <rojerdu.dev@gmail.com>
2024-02-24 10:22:43 -0800

    add table driven tests to queue_test.go

--- queue_test.go
+++ queue_test.go
@@ -12,77 +12,91 @@ func TestQueueInt(t *testing.T) {
 	q.Enqueue(3)
 	q.Enqueue(5)
 
-	fmt.Println(q.elements)
-	el, err := q.Dequeue()
-	if err != nil || el != 2 {
-		t.Errorf("expected 2, got %v", err)
+	fmt.Printf("Current Queue: %v \n", q.elements)
+
+	testCases := []struct {
+		expected int
+	}{
+		{2},
+		{3},
+		{5},
 	}
 
-	el, err = q.Dequeue()
-	if err != nil || el != 3 {
-		t.Errorf("expected 3, got %v", err)
-	}
-
-	el, err = q.Dequeue()
-	if err != nil || el != 5 {
-		t.Errorf("expected 5, got %v", err)
+	for _, tc := range testCases {
+		el, err := q.Dequeue()
+		if err != nil {
+			t.Errorf("unexpeted error: %v", err)
+		}
+		if el != tc.expected {
+			t.Errorf("expected %v, got %v", tc.expected, el)
+		}
 	}
 
 	if !q.IsEmpty() {
-		t.Errorf("expected empty queue")
+		t.Errorf("expected empty queue, got %v", q.elements)
 	}
 }
 
 func TestQueueFloat(t *testing.T) {
 	q := NewQueue()
 
-	q.Enqueue(2.3)
-	q.Enqueue(33.57)
-	q.Enqueue(55.11)
+	q.Enqueue(2.35)
+	q.Enqueue(35.57)
+	q.Enqueue(5711.38)
 
-	el, err := q.Dequeue()
-	if err != nil || el != 2.3 {
-		t.Errorf("expected 2.3, got %v", err)
+	fmt.Printf("Current Queue: %v \n", q.elements)
+
+	testCases := []struct {
+		expected float64
+	}{
+		{2.35},
+		{35.57},
+		{5711.38},
 	}
 
-	el, err = q.Dequeue()
-	if err != nil || el != 33.57 {
-		t.Errorf("expected 33.57, got %v", err)
-	}
-
-	el, err = q.Dequeue()
-	if err != nil || el != 55.11 {
-		t.Errorf("expected 55.11, got %v", err)
+	for _, tc := range testCases {
+		el, err := q.Dequeue()
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if el != tc.expected {
+			t.Errorf("expected %v, got %v", tc.expected, el)
+		}
 	}
 
 	if !q.IsEmpty() {
-		t.Errorf("expected empty queue")
+		t.Errorf("expected empty queue, got %v", q.elements)
 	}
 }
 
 func TestQueueString(t *testing.T) {
 	q := NewQueue()
 
-	q.Enqueue("z")
-	q.Enqueue("y")
-	q.Enqueue("x")
+	q.Enqueue("zyx110")
+	q.Enqueue("abc789")
+	q.Enqueue("mcu123")
 
-	el, err := q.Dequeue()
-	if err != nil || el != "z" {
-		t.Errorf("expected z, got %v", err)
+	fmt.Printf("Current Queue: %v \n", q.elements)
+
+	testCases := []struct {
+		expected string
+	}{
+		{"zyx110"},
+		{"abc789"},
+		{"mcu123"},
 	}
 
-	el, err = q.Dequeue()
-	if err != nil || el != "y" {
-		t.Errorf("expected y, got %v", err)
-	}
-
-	el, err = q.Dequeue()
-	if err != nil || el != "x" {
-		t.Errorf("expected x, got %v", err)
+	for _, tc := range testCases {
+		el, err := q.Dequeue()
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if el != tc.expected {
+			t.Errorf("expected %v, got %v", tc.expected, el)
+		}
 	}
 
 	if !q.IsEmpty() {
-		t.Errorf("expected empty queue")
+		t.Errorf("expected empty queue, got %v", q.elements)
 	}
 }
